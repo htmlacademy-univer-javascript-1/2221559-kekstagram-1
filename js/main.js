df55aae328fc85076c2f712180c51aba8096a5ea
anastaseisha <113300644+anastaseisha@users.noreply.github.com>
2022-09-20 20:50:51 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
-function getRandomNumber(leftNumber,rightNumber)
-//Источник:https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-{
-  if ((leftNumber>=0) && (leftNumber<rightNumber))
-  {
+function getRandomNumber(leftNumber,rightNumber){
+  if ((leftNumber>=0) && (leftNumber<rightNumber)){
     return Math.floor(Math.random() * (rightNumber - leftNumber) + leftNumber);
   }
-  if(leftNumber>rightNumber)
-  {
+  if(leftNumber>rightNumber){
     return Math.floor(Math.random() * (leftNumber - rightNumber) + rightNumber);
   }
-  if (leftNumber===rightNumber)
-  {
+  if (leftNumber===rightNumber){
     return leftNumber;
   }
   return ('error');
 }
-function getMaxLength(line,maxLength)
-{
+function getMaxLength(line,maxLength){
   return (line.length <= maxLength);
 }
 console.log(GetMaxLength('Вася', 5));
