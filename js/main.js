7f2633a8002ff04d12a21eb887d86e157a716427
anastaseisha <a.m.morozova@urfu.me>
2022-10-03 21:51:21 +0500

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -14,19 +14,6 @@ const names = [
   'Маша',
   'Даша' ];
 
-function getRandomNumber(leftNumber,rightNumber){
-  if ((leftNumber >= 0) && (leftNumber < rightNumber)) {
-    return Math.floor(Math.random() * (rightNumber - leftNumber) + leftNumber);
-  }
-  if (leftNumber > rightNumber) {
-    return Math.floor(Math.random() * (leftNumber - rightNumber) + rightNumber);
-  }
-  if (leftNumber === rightNumber){
-    return leftNumber;
-  }
-  return ('error');
-}
-
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
 const getArrayOfNumber = (maxNumber) => {
