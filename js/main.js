6040f0e1302c393359665802d3dc2ad25bf6bc65
anastaseisha <113300644+anastaseisha@users.noreply.github.com>
2022-10-08 16:35:58 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-function getRandomNumber(leftNumber,rightNumber){
-  if ((leftNumber>=0) && (leftNumber<rightNumber)){
+function getRandomNumber(leftNumber, rightNumber) {
+  if ((leftNumber >= 0) && (leftNumber < rightNumber)) {
     return Math.floor(Math.random() * (rightNumber - leftNumber) + leftNumber);
   }
-  if(leftNumber>rightNumber){
+  if(leftNumber > rightNumber){
     return Math.floor(Math.random() * (leftNumber - rightNumber) + rightNumber);
   }
-  if (leftNumber===rightNumber){
+  if (leftNumber === rightNumber) {
     return leftNumber;
   }
   return ('error');
 }
-function getIsMaxLength(line,maxLength){
+function getIsMaxLength(line, maxLength) {
   return (line.length <= maxLength);
 }
-console.log(getMaxLength('Вася', 5));
-console.log(getRandomNumber(1,10));
+getMaxLength('Вася', 5);
+getRandomNumber(1,10);
