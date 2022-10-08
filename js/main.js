0e284433b75a488d97ca7074ab680e9d399f544e
anastaseisha <a.m.morozova@urfu.me>
2022-10-08 17:02:42 +0500

    Больше деталей3

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-function getRandomNumber(leftNumber, rightNumber) {
-  if ((leftNumber >= 0) && (leftNumber < rightNumber)) {
-    return Math.floor(Math.random() * (rightNumber - leftNumber) + leftNumber);
-  }
-  if(leftNumber > rightNumber) {
-    return Math.floor(Math.random() * (leftNumber - rightNumber) + rightNumber);
-  }
-  if (leftNumber === rightNumber) {
-    return leftNumber;
-  }
-  return ('error');
-}
-function getMaxLength(line, maxLength) {
-  return (line.length <= maxLength);
-}
 
-getMaxLength('Длина строки', 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const MESSAGES = [
   'Всё отлично!',
