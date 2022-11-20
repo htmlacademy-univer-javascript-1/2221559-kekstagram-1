c0ae284895813112084db5925d1e34115c097173
Keks <keks@htmlacademy.ru>
2022-11-20 14:59:38 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/util.js
@@ -0,0 +1,26 @@
+function getRandomNumber(leftNumber, rightNumber) {
+  if ((leftNumber >= 0) && (leftNumber < rightNumber)) {
+    return Math.floor(Math.random() * (rightNumber - leftNumber) + leftNumber);
+  }
+  if(leftNumber > rightNumber) {
+    return Math.floor(Math.random() * (leftNumber - rightNumber) + rightNumber);
+  }
+  if (leftNumber === rightNumber) {
+    return leftNumber;
+  }
+  return ('error');
+}
+
+const getArrayOfNumber = (maxNumber) => {
+  const array = [];
+  for (let i = 0; i < maxNumber; i++) {
+    array.push(i + 1);
+  }
+  return array;
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomNumber, getRandomArrayElement, getArrayOfNumber, isEscapeKey};
